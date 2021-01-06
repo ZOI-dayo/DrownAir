7899d2eb328aa0a553421fe51b83666771854381
zoi <zoi@zoizoi.net>
2021-01-06 13:48:31 +0900

    いろんなバグ・不具合を修正 使用可能

--- src/main/java/net/zoizoi/plugin/drownair/logic/PluginLogic.java
+++ src/main/java/net/zoizoi/plugin/drownair/logic/PluginLogic.java
@@ -1,12 +1,13 @@
 package net.zoizoi.plugin.drownair.logic;
 
 import org.bukkit.Material;
-import org.bukkit.block.Block;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
@@ -16,37 +17,44 @@ import java.util.UUID;
 
 public class PluginLogic implements Listener {
     private final JavaPlugin plugin;
-    private final HashMap<UUID, AirLevelDirector> airDitectors = new HashMap<>();
-    private final HashMap<UUID, Integer> remainingAir = new HashMap<>();
+    private final HashMap<UUID, AirLevelDirector> airDirectors = new HashMap<>();
+    private final HashMap<UUID, Integer> inAirTick = new HashMap<>();
 
-    private BukkitRunnable logic = new BukkitRunnable() {
-        private final HashMap<UUID, Integer> inAirTick = new HashMap<>();
+    private final BukkitRunnable logic = new BukkitRunnable() {
+        // private final HashMap<UUID, Integer> inAirTick = new HashMap<>();
 
         @Override
         public void run() {
-            for (AirLevelDirector ditector : airDitectors.values()) {
-                Player player = ditector.getTarget();
+            for (AirLevelDirector director : airDirectors.values()) {
+                Player player = director.getTarget();
                 player.setRemainingAir(player.getMaximumAir());
-                if (player.getEyeLocation().getBlock().getType() == Material.WATER) {
+                boolean isWaterlogged = false;
+                if(player.getEyeLocation().getBlock().getBlockData() instanceof Waterlogged) isWaterlogged = ((Waterlogged)player.getEyeLocation().getBlock().getBlockData()).isWaterlogged();
+                Material block = player.getEyeLocation().getBlock().getType();
+                // plugin.getLogger().info(block.name());
+
+                if (isWaterlogged || block == Material.WATER || block == Material.KELP || block == Material.KELP_PLANT || block == Material.SEAGRASS || block == Material.TALL_SEAGRASS || block == Material.BUBBLE_COLUMN) {
                     inAirTick.put(player.getUniqueId(), 0);
 
-                    airDitectors.get(player.getUniqueId()).setAirticks(0);
+                    airDirectors.get(player.getUniqueId()).setAirticks(0);
                 } else {
-                    if (inAirTick.getOrDefault(player.getUniqueId(), 0) < player.getMaximumAir()) {
-                        inAirTick.put(player.getUniqueId(), inAirTick.getOrDefault(player.getUniqueId(), 0) + 1);
-                    } else {
+                    inAirTick.put(player.getUniqueId(), inAirTick.getOrDefault(player.getUniqueId(), 0) + 1);
+                    if (inAirTick.getOrDefault(player.getUniqueId(), 0) > player.getMaximumAir()) {
                         if(inAirTick.get(player.getUniqueId()) % 20 == 0){
-                            player.damage(1);
+                            player.damage(2);
+                            // plugin.getLogger().info("inAirTick.get(player.getUniqueId()) : " + inAirTick.get(player.getUniqueId()));
                         }
                     }
-                    airDitectors.get(player.getUniqueId()).setAirticks(30 * (10 - inAirTick.getOrDefault(player.getUniqueId(), 0) / 30 - 1) + 3);
+                    airDirectors.get(player.getUniqueId()).setAirticks(30 * (10 - inAirTick.getOrDefault(player.getUniqueId(), 0) / 30 - 1) + 3);
                 }
 
-                // if(airDitectors.get(player.getUniqueId()).getAirticks() == 273)airDitectors.get(player.getUniqueId()).setAirticks(0);
-
+                // if(airDirectors.get(player.getUniqueId()).getAirticks() == 273)airDirectors.get(player.getUniqueId()).setAirticks(0);
+/*
                 plugin.getLogger().info("inAirTick : " + inAirTick.getOrDefault(player.getUniqueId(), 0));
-                plugin.getLogger().info("Airticks : " + airDitectors.get(player.getUniqueId()).getAirticks());
+                plugin.getLogger().info("Airticks : " + airDirectors.get(player.getUniqueId()).getAirticks());
                 plugin.getLogger().info("RemainingAir : " + player.getRemainingAir());
+
+ */
             }
         }
     };
@@ -58,18 +66,18 @@ public class PluginLogic implements Listener {
     }
 
     public void putDrownPlayer(Player player) {
-        if (airDitectors.containsKey(player.getUniqueId())) return;
-        airDitectors.put(player.getUniqueId(), new AirLevelDirector(player));
-        remainingAir.putIfAbsent(player.getUniqueId(), 10);
-        airDitectors.get(player.getUniqueId()).setAirticks(30 * (remainingAir.get(player.getUniqueId()) - 1) + 3);
-        airDitectors.get(player.getUniqueId()).run();
+        if (airDirectors.containsKey(player.getUniqueId())) return;
+        airDirectors.put(player.getUniqueId(), new AirLevelDirector(player));
+        inAirTick.putIfAbsent(player.getUniqueId(), 0);
+        airDirectors.get(player.getUniqueId()).setAirticks(30 * (inAirTick.get(player.getUniqueId()) / 30 - 1) + 3);
+        airDirectors.get(player.getUniqueId()).run();
     }
 
     public void removeDrownPlayer(Player player) {
-        if (!airDitectors.containsKey(player.getUniqueId())) return;
-        airDitectors.get(player.getUniqueId()).finalize();
-        airDitectors.get(player.getUniqueId()).cancel();
-        airDitectors.remove(player.getUniqueId());
+        if (!airDirectors.containsKey(player.getUniqueId())) return;
+        airDirectors.get(player.getUniqueId()).finalize();
+        airDirectors.get(player.getUniqueId()).cancel();
+        airDirectors.remove(player.getUniqueId());
     }
 
     public void End() {
@@ -80,13 +88,18 @@ public class PluginLogic implements Listener {
     @EventHandler
     public void OnPlayerLogin(PlayerJoinEvent e) {
         putDrownPlayer(e.getPlayer());
-        plugin.getLogger().info("OnPlayerLogin");
+        // plugin.getLogger().info("OnPlayerLogin");
     }
 
     @EventHandler
     public void OnPlayerLogout(PlayerQuitEvent e) {
         removeDrownPlayer(e.getPlayer());
-        plugin.getLogger().info("OnPlayerLogout");
+        // plugin.getLogger().info("OnPlayerLogout");
+    }
+
+    @EventHandler
+    public void OnPlayerRespawn(PlayerRespawnEvent e){
+        if(airDirectors.containsKey(e.getPlayer().getUniqueId())) inAirTick.put(e.getPlayer().getUniqueId(), 0);
     }
 
 }
